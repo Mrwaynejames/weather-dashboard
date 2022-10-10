b7d4810055cc8e268c6ed1fec561f32967e6315a
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-10 12:57:45 -0400

    pasr searches are now buttons

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # weather-dashboard
+
+var pastEntryEl = $('#past-entry');
+var cityInputEl = $('#city');
+var listEl = $('li');
+var formEl = $('#user-form');
+
+var printCity = function (city) {
+    var listEl = $('li');
+    listEl.addClass('list-group-item').text(city);
+    listEl.appendto(pastEntryEl);
+};
+
+var weatherForm = function (event) {
+    event.preventDefault();
+
+var cityInput = cityInputEl.val();
+
+    if (!cityInput) {
+        console.log ('fill out criteria');
+        return;
+    }
+
+    printCity(cityInput);
+
+    cityInputEl.val('');
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0665f0648c43bd8f0f67061eb3326c8a";
 var geocodeApi = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},ISO 3166-2:US&limit=1&appid=684fb99c1378d181ad0cdef05eb04ec0";
-formEl.on("submit", weatherForm);
 
-var cities = [""];
+
+var cities = []; 
 function displayCityWeather() {
     var weather = $(this).attr("data-city");
     var queryURL = //open weather API + city + end of URL
 
     $.ajax({
-        url:queryURL,
+        url:weatherApi,
         method: "GET"
-    }).then(function(respobse){
+    }).then(function(response){
+        console.log(response)
         $("#city-weather").empty();
 
         var weatherDiv = $("<div>")
@@ -19,11 +20,12 @@ function displayCityWeather() {
         console.log(response);
         $("city-weather").prepend(weatherDiv);
     })
-}
 
+    renderButtons()
+}
 function renderButtons() {
     $("#past-entry").empty();
-    for (var i=0; i < 3; i++) {
+    for (var i=0; i < cities.length; i++) {
     var newButton = $("<button>");
     newButton.addClass("city-btn");
     newButton.attr("data-city", cities[i]);
@@ -32,12 +34,13 @@ function renderButtons() {
     }
 } 
 
-$("#get-weather").on(submit, function(event) {
+$("#get-weather").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val().trim();
     cities.push(city);
+    
     renderButtons();
 });
 
-$(document).on("submit", ".city-btn", displayCityWeather);
+$(document).on("click", ".city-btn", displayCityWeather);
 renderButtons();
